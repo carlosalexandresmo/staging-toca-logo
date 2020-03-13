f33378ecd643d581201a1cbe31ba3280003ae48a
Carlos <carlos.oliveira@trackage.com.br>
2020-03-13 18:52:27 -0300

    Add migrations

---
+++ database/migrations/2020_02_18_233500_create_show_agendas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShowAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('show_agendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('id_show')->unique();
+            $table->timestamp('start');
+            $table->timestamp('end');
+            $table->string('artistic_name');
+            $table->double('cache');
+            $table->string('music_style');
+            $table->enum('repeat_event', ['DAILY', 'WEEKLY', 'MONTHLY',]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('show_agendas');
+    }
+}
