4c435ce39bf99881ba9e5fe2a15b72a803e7634d
Carlos <carlos.oliveira@trackage.com.br>
2020-03-18 18:05:45 -0300

    Fox

--- config/database.php
+++ config/database.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url("CLEARDB_DATABASE_URL");
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$host = $port = $username = $password = $database = '';
+if(getenv("CLEARDB_DATABASE_URL")){
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $post = $url["port"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
 
 return [
     /*
